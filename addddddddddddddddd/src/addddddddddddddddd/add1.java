4105f89a0d039db1d4d753bd7e1f2325b28deaa4
AnwarSholi <anwar.ghassan.sholi@gmail.com>
2020-02-22 19:22:57 +0200

    add

---
+++ addddddddddddddddd/src/addddddddddddddddd/add1.java
@@ -0,0 +1,25 @@
+package addddddddddddddddd;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class add1 {
+	public static int add(int a, int b){
+		int d;
+		d = a+b;
+		return d;
+	}
+	public static void main(String[] args) throws IOException {
+		int c;
+		int b;
+		Scanner in = new Scanner(System.in);
+		b = in.nextInt();
+		int a;
+		a = in.nextInt();
+		if(b >0 && a>0){
+			c = add(b,a);
+			System.out.print(c);
+		}
+		else
+			System.out.println("a<0 or b<0 ");
+	}
+}
