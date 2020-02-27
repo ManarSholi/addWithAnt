67b892a4377057612b37889a9939d710345ad364
AnwarSholi <anwar.ghassan.sholi@gmail.com>
2020-02-27 22:25:04 +0200

    calculator

---
+++ CalcAdd/Test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+package main.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+}
